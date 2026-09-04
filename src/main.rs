0d1c40016209078529fe861fd450c4c1a75b9873
tisupe <luna.sharma7894@gmail.com>
2026-09-04 19:10:29 +0530

    ooooo

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
-fn main() {}
+use std::io::Write;
+use std::net::TcpListener;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                stream
+                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
